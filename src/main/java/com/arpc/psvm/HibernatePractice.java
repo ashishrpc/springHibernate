7fc7378a2f3b32aac35443fe504293b2ca4117e1
ashish.prasad <ashish.prasad@KMG-016.kmg.com>
2017-10-03 16:26:52 +0530

    CRUD can perform by createQuery()

--- src/main/java/com/arpc/psvm/HibernatePractice.java
+++ src/main/java/com/arpc/psvm/HibernatePractice.java
@@ -1,5 +1,8 @@
 package com.arpc.psvm;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -17,6 +20,28 @@ public class HibernatePractice {
 		Person person =(Person)session.get(Person.class, 1);
 		System.out.println(person);
 		session.close();
+		//update from query
+		session=sessionFactory.openSession();
+		Query query=session.createQuery("update from Person set country=? where id=:myId");
+		query.setString(0, "india");
+		query.setLong("myId", 1);
+		int i=query.executeUpdate();
+		System.out.println("total update: "+i);
+		session.close();
+		//delete from query
+//		session=sessionFactory.openSession();
+//		query=session.createQuery("delete from Person where id=:myId");
+//		query.setLong("myId", 1);
+//		i=query.executeUpdate();
+//		System.out.println("total update: "+i);
+//		session.close();
+		//select data from creteria
+		session=sessionFactory.openSession();
+		query=session.createQuery("from Person where id=?");
+		query.setLong(0, 1);
+		List<Person> personList=query.list();
+		System.out.println(personList);
+		
 		context.registerShutdownHook();
 	}
 
