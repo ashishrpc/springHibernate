187ba0d80b7b03b2f2b7db1366b23a9aaeb595a3
ashishrpc <ashishrpc@gmail.com>
2018-03-03 19:40:18 +0530

    OutOfMemory, Heap memory, Stackoverflow, and thread generate too much and find these issue by Profiler, VisualVM

--- src/main/java/com/arpc/spring/HomeController.java
+++ src/main/java/com/arpc/spring/HomeController.java
@@ -1,8 +1,13 @@
 package com.arpc.spring;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +27,19 @@ public class HomeController {
 		//adding some time lag to check interceptor execution
 		try {
 			Thread.sleep(1000);
+			//out of memory
+			//generateOOM();
+			//memoryLeakage();
+			//givenMap_whenNoEqualsNoHashCodeMethods_thenOutOfMemory();
+			
+			//stack OverFlow
+			//recursivePrint(1);
+			
+			//Thread max creation
+			CreateThreads.main(null);
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		Date date = new Date();
@@ -34,4 +51,108 @@ public class HomeController {
 		logger.info("Before returning view page");
 		return "home";
 	}
+	
+	public void generateOOM() throws Exception {
+		int iteratorValue = 20;
+		System.out.println("\n=================> OOM test started..\n");
+		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+			System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
+			int loop1 = 2;
+			int[] memoryFillIntVar = new int[iteratorValue];
+			// feel memoryFillIntVar array in loop..
+			do {
+				memoryFillIntVar[loop1] = 0;
+				loop1--;
+			} while (loop1 > 0);
+			iteratorValue = iteratorValue * 5;
+			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
+			Thread.sleep(1000);
+		}
+	}
+	public void memoryLeakage() throws Exception{
+		for(int i=0;i<100;i++){
+			URL url = new URL("ftp://speedtest.tele2.net");
+		    URLConnection urlc = url.openConnection();
+		    InputStream is = urlc.getInputStream();
+		    String str = "";
+		}
+	}
+	public void givenMap_whenNoEqualsNoHashCodeMethods_thenOutOfMemory()
+			throws IOException {
+		Map<Object, Object> map = System.getProperties();
+		while (true) {
+			map.put(new Key("key"), "value");
+		}
+	}
+	public void recursivePrint(int num) {//stackOverFlowError
+		System.out.println("Number: " + num);
+		if(num == 0)
+			return;
+		else
+			recursivePrint(++num);
+	}
+
 }
+class Key {
+    public String key;
+    
+    public Key(String key) {
+        this.key = key;
+    }
+}
+
+///Max thread created on JVM
+class CreateThreads {  
+	/** 
+	 * Thread class 
+	 * 
+	 */  
+	private static final class MyThread extends Thread  
+	{  
+		private final int number;  
+		private final String test;
+		public MyThread(int number,String test) {  
+			this.number = number;  
+			this.test=test+"~"+number;
+		}  
+		@Override  
+		public void run() {  
+			if (shouldPrintMessage(number))  
+			{  
+				System.out.println("Thread no. "+number+" started.");  
+			}  
+			try {  
+				//sleep forever  
+				Thread.sleep(Long.MAX_VALUE);  
+			} catch (InterruptedException e) {  
+				e.printStackTrace();  
+			}  
+		}  
+	}  
+	/** 
+	 * @param args 
+	 */  
+	public static void main(String[] args) {  
+		long startTime = System.currentTimeMillis();   
+		final int noOfThreads = 1000;//1000000;  
+		int i=0;  
+		try {  
+			for (i=0; i < noOfThreads; i++)  
+			{  
+				if (shouldPrintMessage(i))  
+				{  
+					System.out.println("Creating thread "+i+" ("+(System.currentTimeMillis()-startTime)+"ms)");  
+				}  
+				new MyThread(i,"Ashish ").start();  
+			}  
+		} catch (Throwable e) {  
+			System.out.println("Error thrown when creating thread "+i);  
+			e.printStackTrace();  
+		}  
+	}  
+
+	private static boolean shouldPrintMessage(int i)  
+	{  
+		return i % 100 == 0;  
+	}  
+}  
